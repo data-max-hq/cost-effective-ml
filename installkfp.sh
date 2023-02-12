353635e17b6768efd12bc876cedd5ab7ff2fb5ba
Sadik Bakiu <sbakiu@gmail.com>
2023-02-12 16:03:57 +0100

    Initial setup for kubeflow pipelines

--- installkfp.sh
+++ installkfp.sh
@@ -4,3 +4,8 @@ export PIPELINE_VERSION=1.8.5
 kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/cluster-scoped-resources?ref=$PIPELINE_VERSION"
 kubectl wait --for condition=established --timeout=60s crd/applications.app.k8s.io
 kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/env/dev?ref=$PIPELINE_VERSION"
+
+# delete
+#export PIPELINE_VERSION=1.8.5
+#kubectl delete -k "github.com/kubeflow/pipelines/manifests/kustomize/env/dev?ref=$PIPELINE_VERSION"
+#kubectl delete -k "github.com/kubeflow/pipelines/manifests/kustomize/cluster-scoped-resources?ref=$PIPELINE_VERSION"
