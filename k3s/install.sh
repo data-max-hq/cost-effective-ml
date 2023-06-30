6170e52ce5e74e566468cf2ec01d796097e4ed30
Sadik Bakiu <sbakiu@gmail.com>
2023-06-30 16:06:59 +0200

    restructure repo

--- k3s/install.sh
+++ k3s/install.sh
@@ -1,107 +1,98 @@
-# Create organization tags
-
-# Create firewall rule
-#
-
-# Create instances with tags
-# 4 CPU 16 GB 100 GB
+# Install requirements
 sudo apt-get update -y && sudo apt-get upgrade -y
 
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 && chmod 700 get_helm.sh && ./get_helm.sh
 
+sudo apt-get install apt-transport-https git make -y
 
-# install customize
+# install kustomize
 curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
 sudo mv kustomize /bin/
 
-## Prepare registries
-sudo mkdir -p /etc/rancher/k3s
-sudo vi /etc/rancher/k3s/registries.yaml
-
-## Install nvidia-container-toolkit
+# Install nvidia-container-toolkit
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
     && curl -s -L https://nvidia.github.io/libnvidia-container/gpgkey | sudo apt-key add - \
     && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list \
     && sudo apt-get update \
     && sudo apt-get install -y nvidia-container-toolkit
 
-# server
-sudo systemctl restart k3s
-# restart agent
-sudo systemctl restart k3s-agent
 
-/usr/local/bin/k3s-uninstall.sh
-/usr/local/bin/k3s-agent-uninstall.sh
-
-# Install k3s main, only in master
-# curl -sfL https://get.k3s.io | sh -
-#--node-external-ip=34.31.109.232 --flannel-backend=wireguard-native --flannel-external-ip
-#
-#curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.25.8+k3s1 INSTALL_K3S_EXEC="--node-external-ip=147.189.199.227 --flannel-backend=wireguard-native --flannel-external-ip --prefer-bundled-bin" sh -
-
+# Install k3s
 curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.25.8+k3s1 sh -
 
+# Install GPU Operator
+sudo helm repo add nvidia https://helm.ngc.nvidia.com/nvidia \
+   && sudo helm repo update
 
-# Get node token
-# sudo cat /var/lib/rancher/k3s/server/node-token
-export K3S_NODE_TOKEN=$(sudo cat /var/lib/rancher/k3s/server/node-token)
+sudo helm upgrade --install --wait gpu-operator \
+     --namespace gpu-operator \
+     --create-namespace \
+      nvidia/gpu-operator \
+      --set driver.enabled=false \
+      --set toolkit.enabled=false \
+      --kubeconfig /etc/rancher/k3s/k3s.yaml
 
-SERVER_IP=
-K3S_NODE_TOKEN=
-curl -sfL https://get.k3s.io | K3S_URL=https://${SERVER_IP}:6443 K3S_TOKEN=${K3S_NODE_TOKEN} sh -
+# Check installation
 
-# Dashboard
-# https://docs.k3s.io/installation/kube-dashboard
-GITHUB_URL=https://github.com/kubernetes/dashboard/releases
-VERSION_KUBE_DASHBOARD=$(curl -w '%{url_effective}' -I -L -s -S ${GITHUB_URL}/latest -o /dev/null | sed -e 's|.*/||')
-sudo k3s kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/${VERSION_KUBE_DASHBOARD}/aio/deploy/recommended.yaml
+#sudo helm list -n gpu-operator --kubeconfig /etc/rancher/k3s/k3s.yaml
+#sudo helm uninstall gpu-operator -n gpu-operator --kubeconfig /etc/rancher/k3s/k3s.yaml
+sudo kubectl get po -n gpu-operator
 
-vi user.yaml
-vi role.yaml
+sudo kubectl describe nodes
 
-sudo k3s kubectl create -f user.yaml -f role.yaml
-
-sudo k3s kubectl -n kubernetes-dashboard create token admin-user
-
-sudo k3s kubectl port-forward svc/kubernetes-dashboard  -n kubernetes-dashboard 8443:443 --address='0.0.0.0'
-
-## Add node labels
-sudo k3s kubectl get nodes --show-labels
-sudo k3s kubectl label nodes k3s-instance-2 cpu=true
-sudo k3s kubectl label nodes k3s-instance-3 cpu=true
-sudo k3s kubectl label nodes k3s-instance-4 gpu=true
-sudo k3s kubectl label nodes k3s-instance-5 gpu=true
-
-
-# https://github.com/k3s-io/k3s/issues/4391#issuecomment-1233314825
-
-## install kuberay operator
-
-# Kubeflow Pipelines
-# export PIPELINE_VERSION=1.8.5
-#sudo k3s kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/cluster-scoped-resources?ref=$PIPELINE_VERSION"
-#sudo k3s kubectl wait --for condition=established --timeout=60s crd/applications.app.k8s.io
-#sudo k3s kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/env/dev?ref=$PIPELINE_VERSION"
+# - Install nvidia K8S Device plugin
+#sudo helm repo add nvdp https://nvidia.github.io/k8s-device-plugin
+#  sudo helm repo update
+#  sudo helm upgrade -i nvdp nvdp/nvidia-device-plugin \
+#  --namespace nvidia-device-plugin \
+#  --create-namespace \
+#  --version 0.14.0 \
+#  --kubeconfig /etc/rancher/k3s/k3s.yaml
 #
+## check installation
+#sudo kubectl get po -n nvidia-device-plugin
 #
-#sudo k3s kubectl port-forward svc/ml-pipeline-ui  -n kubeflow 8080:80 --address='0.0.0.0'
+#sudo helm uninstall nvdp -n nvidia-device-plugin --kubeconfig /etc/rancher/k3s/k3s.yaml
 
-##install kubeflow
-#git clone https://github.com/kubeflow/manifests.git
+
+# Install Kubeflow
 git clone https://github.com/data-max-hq/manifests.git
 cd manifests/
 while ! kustomize build example | awk '!/well-defined/' | sudo k3s kubectl apply -f -; do echo "Retrying to apply resources"; sleep 10; done
 
-sudo k3s kubectl port-forward svc/istio-ingressgateway -n istio-system 8080:80 --address='0.0.0.0'
+sudo kubectl get po -n kubeflow
 
-# enable unsecure
-# https://github.com/kubeflow/manifests#change-default-user-password
-https://github.com/kubeflow/manifests/pull/2155
-https://github.com/kubeflow/manifests/issues/2225#issuecomment-1157931840
+sudo kubectl port-forward svc/istio-ingressgateway -n istio-system 8080:80 --address='0.0.0.0'
 
-# Change username password
-https://github.com/kubeflow/manifests#change-default-user-password
+# Install kuberay-operator
+sudo helm repo add kuberay https://ray-project.github.io/kuberay-helm/
+sudo helm repo update
+sudo helm upgrade --install \
+  kuberay-operator kuberay/kuberay-operator \
+  --version 0.5.0 \
+  --kubeconfig /etc/rancher/k3s/k3s.yaml
 
-# delete kubeflow
-kustomize build example | sudo k3s kubectl delete -f -
+# Check the KubeRay operator Pod in `default` namespace
+sudo k3s kubectl get pods
 
-sudo k3s kubectl port-forward svc/demo-cluster-head-svc  8625:8625 --address='0.0.0.0'
+sudo k3s kubectl get svc
+
+
+#Install cluster
+sudo kubectl apply -f cluster.yaml
+
+sudo k3s kubectl port-forward --address 0.0.0.0 svc/example-cluster-head-svc 8265:8265 -n kubeflow-user-example-com
+
+
+sudo kubectl exec -it -n kubeflow-user-example-com example-cluster-head-7ddxf -- /bin/bash
+
+wget https://raw.githubusercontent.com/ray-project/ray/94062557ffea29530da236a7700d51e511f61906/python/ray/train/examples/tf/tensorflow_mnist_example.py
+
+time python tensorflow_mnist_example.py --num-workers 2 --use-gpu True --epochs 3
+
+# Install agent nodes
+sudo cat /var/lib/rancher/k3s/server/node-token
+
+SERVER_IP=192.168.17.171
+K3S_NODE_TOKEN=K104c90d5cec4a63e8ac012057d0dc605d7a1be06b87244ddcf82a569dabd354a2a::server:76d1e6d0c864210a1c81827a93334c0f
+curl -sfL https://get.k3s.io | K3S_URL=https://${SERVER_IP}:6443 K3S_TOKEN=${K3S_NODE_TOKEN} sh -
