d54f20964b23d43e645b6c049001c5612bba51a0
Sadik Bakiu <sbakiu@gmail.com>
2023-10-11 21:09:59 +0200

    Add commands

--- ray_demo/install.sh
+++ ray_demo/install.sh
@@ -1,9 +1,13 @@
 # Install before python
-sudo apt-get install gpg-agent -y
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install gpg-agent git -y
+
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt update
 
 # Install Python: https://www.linuxcapable.com/install-python-3-8-on-ubuntu-linux/
 sudo apt install python3.9-distutils python3.9 -y
-
 
 # Install pip
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
@@ -24,6 +28,10 @@ pip install "ray[tune]"
 ray start --head --port=6379 --dashboard-host=0.0.0.0
 ray start --address=192.168.10.129:6379
 
+git clone https://github.com/data-max-hq/cost-effective-ml.git
+
 mkdir ray
 cd ray
 vi mnist.py
+
+RAY_ADDRESS='http://192.168.10.129:8265' ray job submit --working-dir . -- python3.9 mnist.py
