68dee602ac48533092f1de9982aa8c0a7ab9d872
Sadik Bakiu <sbakiu@gmail.com>
2023-10-15 22:44:04 +0200

    Clean up

--- ray_demo/install.sh
+++ ray_demo/install.sh
@@ -1,35 +1,5 @@
-# Install before python
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install gpg-agent git -y
-
-sudo add-apt-repository ppa:deadsnakes/ppa -y
-sudo apt update
-
-# Install Python: https://www.linuxcapable.com/install-python-3-8-on-ubuntu-linux/
-sudo apt install python3.9-distutils python3.9 -y
-
-# Install pip
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3.9 get-pip.py
-
-
-export PATH=$PATH:/home/ubuntu/.local/bin
-
-# Install ray
-pip install -U "ray[default]"
-
-pip install torch torchvision torchaudio tqdm pandas
-pip install "ray[train]"
-pip install "ray[tune]"
-pip install notebook
-pip install tensorboard
-pip install ipywidgets
-
-pip install jupyterlab
 jupyter lab --ip 0.0.0.0
 
-pip install notebook
 jupyter notebook --ip 0.0.0.0
 
 # Start ray cluster
@@ -37,9 +7,5 @@ ray start --head --port=6379 --dashboard-host=0.0.0.0
 ray start --address=192.168.11.161:6379
 
 git clone https://github.com/data-max-hq/cost-effective-ml.git
-
-mkdir ray
-cd ray
-vi mnist.py
 
 RAY_ADDRESS='http://192.168.11.161:8265' ray job submit --working-dir . -- python3.9 mnist.py 2
---
+++ ray_demo/prepare_machine.md
@@ -0,0 +1,36 @@
+# Prepare Machine
+## Install before python
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install gpg-agent git -y
+
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt update
+
+## Install Python: https://www.linuxcapable.com/install-python-3-8-on-ubuntu-linux/
+sudo apt install python3.9-distutils python3.9 -y
+
+## Install pip
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3.9 get-pip.py
+
+## Modify Path
+```
+export PATH=$PATH:/home/ubuntu/.local/bin
+```
+
+## Install ray and dependencies
+```
+pip install torch torchvision torchaudio tqdm pandas
+pip install "ray[default]"
+pip install "ray[train]"
+pip install "ray[tune]"
+pip install tensorboard
+pip install ipywidgets
+pip install jupyterlab
+pip install notebook
+```
+or
+```commandline
+pip install -r requirements.txt
+```
