3d23f26faee6040b8bb7eb59c9f0a811a406bb81
Sadik Bakiu <sbakiu@gmail.com>
2023-12-05 15:02:32 +0100

    Small updates

--- ray_demo/install.sh
+++ ray_demo/install.sh
@@ -1,11 +1,11 @@
+git clone https://github.com/data-max-hq/cost-effective-ml.git
+
 jupyter lab --ip 0.0.0.0
 
 jupyter notebook --ip 0.0.0.0
 
 # Start ray cluster
-ray start --head --port=6379 --dashboard-host=0.0.0.0
+ray start --head --port=6379 --dashboard-host=0.0.0.0 --disable-usage-stats
 ray start --address=192.168.11.179:6379
-
-git clone https://github.com/data-max-hq/cost-effective-ml.git
 
 RAY_ADDRESS='http://192.168.11.179:8265' ray job submit --working-dir . -- python3.9 mnist.py 2
