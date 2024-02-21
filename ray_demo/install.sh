3c113e02e6fa56354b6762dcbefcae67cbefff2a
Sadik Bakiu <sbakiu@gmail.com>
2024-02-21 10:57:44 +0100

    Small updates

--- ray_demo/install.sh
+++ ray_demo/install.sh
@@ -6,6 +6,6 @@ jupyter notebook --ip 0.0.0.0
 
 # Start ray cluster
 ray start --head --port=6379 --dashboard-host=0.0.0.0 --disable-usage-stats
-ray start --address=192.168.10.160:6379
+ray start --address=192.168.11.100:6379
 
-RAY_ADDRESS='http://192.168.10.160:8265' ray job submit --working-dir . -- python3.9 mnist.py 2
+RAY_ADDRESS='http://192.168.11.100:8265' ray job submit --working-dir . -- python3.9 mnist.py 2
