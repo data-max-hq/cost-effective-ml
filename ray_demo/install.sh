3c7b07b883dad36fd796632bd635d9762b0b9dfd
Sadik Bakiu <sbakiu@gmail.com>
2024-04-25 15:56:10 +0200

    Clean up

--- ray_demo/install.sh
+++ ray_demo/install.sh
@@ -8,4 +8,4 @@ jupyter notebook --ip 0.0.0.0
 ray start --head --port=6379 --dashboard-host=0.0.0.0 --disable-usage-stats
 ray start --address=192.168.11.100:6379
 
-RAY_ADDRESS='http://192.168.11.100:8265' ray job submit --working-dir . -- python3.9 mnist.py 2
+RAY_ADDRESS='http://192.168.10.65:8265' ray job submit --working-dir . -- python3.10 mnist.py 2
